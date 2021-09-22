e330585be969419daf4568d4ab72610b6616eb12
Lospub <zhjyy002@gmail.com>
2021-09-22 12:28:50 -0600

    add the basic template

---
+++ icd_tree/src/index.js
@@ -0,0 +1,46 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import ReactDom from 'react-dom';
+import $ from 'jquery';
+import Header from './components/header';
+import TreeContainer from './components/treeContainer';
+import json from './json';
+import Store from './Reducers/store';
+import { connect, Provider } from 'react-redux';
+import { resize } from './Reducers/actions';
+
+import './index.css';
+
+$(window).on('resize', resize);
+
+const propTypes = {
+	activeNode: PropTypes.string,
+	filter: PropTypes.string.isRequired,
+	height: PropTypes.number.isRequired,
+	width: PropTypes.number.isRequired
+};
+
+class App extends React.PureComponent {
+	render() {
+		return (
+			<div id="container">
+				<Header filter={this.props.filter} timestamp={Date().toLocaleString()}/>
+				<TreeContainer
+					activeNode={this.props.activeNode}
+					data={json}
+					filter={this.props.filter}
+					height={this.props.height}
+					width={this.props.width}/>
+			</div>);
+	}
+}
+
+App.propTypes = propTypes;
+App = connect(state => state)(App);
+
+ReactDom.render(
+	<Provider store={Store}>
+		<App/>
+	</Provider>,
+	document.getElementById('root')
+);
---
+++ icd_tree/src/json.js
@@ -0,0 +1,59 @@
+export default {
+
+	name: 'I',
+	children: [{
+		name: 'I00',
+		children: []
+	}, {
+		name: 'I01',
+		children: [{
+			name: 'I01.0',
+			children: [{
+				name: 'I01.00',
+				children: []
+			}, {
+				name: 'I01.01',
+				children: []
+			}]
+		}, {
+			name: 'I01.1',
+			children: []
+		}, {
+			name: 'I01.2',
+			children: []
+		}, {
+			name: 'I01.3',
+			children: []
+		}]
+	}, {
+		name: 'I02',
+		children: []
+	}, {
+		name: 'I03',
+		children: [{
+			name: 'I03.0',
+			children: []
+		}, {
+			name: 'I03.1',
+			children: []
+		}]
+	}, {
+		name: 'I04',
+		children: [{
+			name: 'I04.0',
+			children: []
+		}, {
+			name: 'I04.1',
+			children: []
+		}, {
+			name: 'I04.2',
+			children: []
+		}]
+	}, {
+		name: 'I05',
+		children: []
+	}, {
+		name: 'I06',
+		children: []
+	}]
+};
