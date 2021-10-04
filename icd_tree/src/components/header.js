ebd3c8aa88365fbd2ad3fbe0e16d5a830969f111
Lospub <zhjyy002@gmail.com>
2021-10-04 11:05:29 -0600

    upload CSV file

--- icd_tree/src/components/header.js
+++ icd_tree/src/components/header.js
@@ -2,6 +2,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Filter from './filter';
 import { setActiveNode, setFilter, resize } from '../Reducers/actions';
+import { CSVReader } from 'react-papaparse';
+
+const buttonRef = React.createRef();
 
 const propTypes = {
 	filter: PropTypes.string.isRequired,
@@ -18,17 +21,91 @@ export default class Header extends React.PureComponent {
 		setFilter('');
 	}
 
-	// handleUpload() {
-	// 	alert(`This is the feature for upload CSV file`);
-	// }
+	handleOpenDialog = (e) => {
+		// Note that the ref is set async, so it might be null at some point
+		if (buttonRef.current) {
+		  buttonRef.current.open(e);
+		}
+	  };
+	
+	  handleOnFileLoad = (data) => {
+		console.log('---------------------------');
+		console.log(data);
+		console.log('---------------------------');
+	  };
+	
+	  handleOnError = (err, file, inputElem, reason) => {
+		console.log('---------------------------');
+		console.log(err);
+		console.log('---------------------------');
+	  };
+	
+	  handleOnRemoveFile = (data) => {
+		console.log('---------------------------');
+		console.log(data);
+		console.log('---------------------------');
+	  };
+	
+	  handleRemoveFile = (e) => {
+		// Note that the ref is set async, so it might be null at some point
+		if (buttonRef.current) {
+		  buttonRef.current.removeFile(e);
+		}
+	  };
 
 	render() {
 		return (
 			<div id="header">
 				<Filter filter={this.props.filter}/>
-				<button onClick={this.handleClick}>Reset</button>
-				{/* <button onClick={this.handleClick}>Upload CSV</button> */}
+				<button onClick={this.handleClick}>Reset Tree</button>
 				<span>Last Updated: {this.props.timestamp}</span>
+				<CSVReader
+					ref={buttonRef}
+					onFileLoad={this.handleOnFileLoad}
+					onError={this.handleOnError}
+					noClick
+					noDrag
+					onRemoveFile={this.handleOnRemoveFile}
+					>
+					{({ file }) => (
+						<aside
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+						}}
+						>
+						<button
+							style={{marginTop:5}}
+							onClick={this.handleOpenDialog}
+						>
+							Browse file
+						</button>
+						<div
+							style={{
+							borderWidth: 1,
+							background:'#F4F4F4',
+							borderStyle: 'solid',
+							borderColor: '#ccc',
+							borderRadius: 50,
+							height: 22,
+							marginLeft: 5,
+							marginTop: 5,
+							paddingLeft: 13,
+							paddingTop: 3,
+							width: '60%',
+							}}
+						>
+							{file && file.name}
+						</div>
+						<button
+							style={{marginTop: 5}}
+							onClick={this.handleRemoveFile}
+						>
+							Remove
+						</button>
+						</aside>
+					)}
+					</CSVReader>
 			</div>);
 	}
 }
